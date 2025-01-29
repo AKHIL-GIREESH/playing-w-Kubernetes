5a2bed79153ed0d9029f94fb5c564686f8989d08
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2025-01-29 05:39:36 +0530

    testApp

--- testApp1/expressApp/index.js
+++ testApp1/expressApp/index.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 
 const app = express();
 
-app.get("/",(req,res) => res.status(200).send(`<h1>This is a Container </h1>`) );
+app.get("/",(req,res) => res.status(200).send(`<h1>This is a Container running in a Pod </h1>`) );
 
 app.listen(3000,() => console.log("Server is Up and running 🚀"));
 
